71a724a657df58183099d351e536dbc3f29a5772
wayfarer <genia.sushko@gmail.com>
2015-01-21 20:16:15 +0200

    Added Mythreads2 (starting of 50 threads from another thread and interrupting them).

---
+++ Base20/Base20/src/week7/lesson13/Mythreads2.java
@@ -0,0 +1,57 @@
+package week7.lesson13;
+
+import java.util.ArrayList;
+
+import week7.lesson13.MyThreads.Test;
+
+/*
+ * Task 2. Создать поток, который создаст 50 потоков, каждый их которых выведет на экран свой номер и дождется, пока его прервут. 
+ * Прерывание дочерних потоков должно выполнятся из потока их порождающего.
+ */
+
+public class Mythreads2 {
+	public static class SuperThread extends Thread {
+		String name = "Main thread ";
+
+		SuperThread(String name) {
+			this.name += name;
+		}
+
+		public void run() {
+			System.out.println(name);
+
+			ArrayList<SecondaryThread> threads = new ArrayList<>();
+			
+			for (int i = 0; i < 50; i++) {
+				threads.add(new SecondaryThread("" + i));
+				threads.get(i).start();
+			}
+
+			for (SecondaryThread thread : threads) {
+				thread.interrupt();
+			}
+		}
+	}
+
+	public static class SecondaryThread extends Thread {
+		String name = "Secondary thread ";
+
+		SecondaryThread(String name) {
+			this.name += name;
+		}
+
+		public void run() {
+			System.out.println(name);
+			while (true)
+				if (isInterrupted()) {
+					System.out.println(name + " - interrupted");
+					return;
+				}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Thread thr = new SuperThread("");
+		thr.start();
+	}
+}
