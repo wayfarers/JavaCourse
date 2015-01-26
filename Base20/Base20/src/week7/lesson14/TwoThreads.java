698abf71d150ea4fd932f14a651825704142fef4
wayfarer <genia.sushko@gmail.com>
2015-01-26 17:02:41 +0200

    Updated TwoThreads task (seems to be completed)

--- Base20/Base20/src/week7/lesson14/TwoThreads.java
+++ Base20/Base20/src/week7/lesson14/TwoThreads.java
@@ -1,6 +1,7 @@
 package week7.lesson14;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -26,58 +27,138 @@ public class TwoThreads {
 		File file1, file2;
 		Thread writer, copier;
 		
-		public Monitor(String nameFile1, String nameFile2) throws IOException {
+		public Monitor(String nameFile1, String nameFile2) throws Exception {
 			file1 = new File(nameFile1);
 			file2 = new File(nameFile2);
-			writer = new ThreadCreator(file1);
+			writer = new ThreadCreator(file1, this);
 			copier = new ThreadWriter(file1, file2);
-			
+		}
+		
+		public void start() {
 			writer.start();
 			copier.start();
 		}
+		
+		public void stop() {
+			if (writer.isAlive())
+				writer.interrupt();
+			if (copier.isAlive())
+				copier.interrupt();
+		}
+		
+		
 	}
 	
 	public static class ThreadCreator extends Thread {
 		Scanner scan = new Scanner(System.in);
 		String line;
 		FileWriter toFile;
+		File file1;
+		Monitor mon;
 
-		public ThreadCreator(File file1) throws IOException {
+		public ThreadCreator(File file1, Monitor mon) throws Exception {
+			this.file1 = file1;
+			this.mon = mon;
 			if (!file1.exists()) 
 				file1.createNewFile();
-			toFile = new FileWriter(file1);
-
 		}
 
 		public void run() {
 			while (!isInterrupted()) {
-				synchronized (toFile) {
+				synchronized (file1) {
+					System.out.println("Thread 1 working");
 					if (scan.hasNextLine()) {
 						line = scan.nextLine();
-						if (line.equals("quit") || line.equals("exit")) {
-							//остановить потоки
-							return;
+						if (line.equals("quit") | line.equals("exit")) {
+							mon.stop();
 						}
 					}
-					
+					try {
+						toFile = new FileWriter(file1, true);
+						toFile.write(line + "\n");
+						toFile.close();
+					} catch (IOException e) {
+						System.out.println("Writing error.");
+						e.printStackTrace();
+						return;
+					}
+					file1.notify();
 				}
 			}
+			System.out.println("Thread 1: interrupted");
 		}
 	}
 	
 	public static class ThreadWriter extends Thread {
+		int symbol;
+		FileWriter toFile;
+		FileReader fromFile; 
+		File file1, file2;
 		
 		public ThreadWriter(File fromFile, File toFile) {
-			
+			file1 = fromFile;
+			file2 = toFile;
 		}
 
 		public void run() {
-
+			while (!isInterrupted()) {
+				synchronized (file1) {
+					System.out.println("\tThread 2 working");
+					if (!file1.exists())
+						try {
+							file1.wait();
+							System.out.println("/tWaiting");
+						} catch (InterruptedException e) {
+							System.out.println("Thread waiting error");
+							e.printStackTrace();
+							return;
+						}
+					
+					try {
+						fromFile = new FileReader(file1);
+						toFile = new FileWriter(file2);
+					} catch (FileNotFoundException e) {
+						System.out.println("Thread 2: file not found");
+						e.printStackTrace();
+						return;
+					} catch (IOException e) {
+						System.out.println("Thread 2: error writing file");
+						e.printStackTrace();
+						return;
+					}
+					
+					try {
+						while ((symbol = fromFile.read()) != -1) {
+							toFile.write(symbol);
+						}
+						fromFile.close();
+					} catch (IOException e) {
+						System.out.println("Thread 2: error reading file");
+						e.printStackTrace();
+						return;
+					}
+					try {
+						file1.wait();
+					} catch (InterruptedException e) {
+						break;
+					}
+				}
+			}
+			try {
+				toFile.close();
+			} catch (IOException e) {
+				System.out.println("Thread 2: error closing file");
+				e.printStackTrace();
+			}
+			System.out.println("Thread 2: interrupted");
 		}
 
 	}
 	
-	 public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception {
+		Monitor test = new Monitor("file1.txt", "file2.txt");
+		test.start();
+		System.out.println("Program finished");
+
 	}
 }
