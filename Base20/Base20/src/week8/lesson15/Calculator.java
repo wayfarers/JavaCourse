620f015a2d4a4c1ac3502d5bbdae9ea07d91025e
wayfarer <genia.sushko@gmail.com>
2015-01-27 00:24:11 +0200

    Nothing important

--- Base20/Base20/src/week8/lesson15/Calculator.java
+++ Base20/Base20/src/week8/lesson15/Calculator.java
@@ -8,7 +8,6 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.Box;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
